3021897927548f61d270f5dd46d0303246d40a4b
manueladuque-svg <manuela.duque@est.iudigital.edu.co>
2026-09-17 17:40:56 -0500

    Update IngredientesInvalidosException.java

--- src/IngredientesInvalidosException.java
+++ src/IngredientesInvalidosException.java
@@ -1,10 +1,4 @@
-/**
- * Excepción personalizada para controlar la validación del arreglo de ingredientes
- * en el sistema Pizza-Track.
- * 
- * Se lanza cuando el arreglo de ingredientes no cumple con la restricción obligatoria
- * de tener exactamente 3 ingredientes válidos y no vacíos.
- */
+// Excepcion para validacion de los 3 ingredientes requeridos
 public class IngredientesInvalidosException extends Exception {
 
     public IngredientesInvalidosException(String mensaje) {
